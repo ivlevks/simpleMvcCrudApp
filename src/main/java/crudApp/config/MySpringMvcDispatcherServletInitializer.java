2a9521e057c612e203ff4c3634699e6acc6db92a
ivlevks <ivlevks@yandex.ru>
2021-08-16 21:50:24 +0300

    created DAO, models and controller

--- src/main/java/crudApp/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/crudApp/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,4 +1,4 @@
-package simpleCrud.config;
+package crudApp.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
--- src/main/java/crudApp/config/SpringConfig.java
+++ src/main/java/crudApp/config/SpringConfig.java
@@ -1,4 +1,4 @@
-package simpleCrud.config;
+package crudApp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -13,7 +13,7 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
-@ComponentScan("simpleCrud.config")
+@ComponentScan("crudApp")
 @EnableWebMvc
 public class SpringConfig implements WebMvcConfigurer {
 
